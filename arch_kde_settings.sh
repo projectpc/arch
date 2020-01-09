37ab16cd0cb2861e2f5b00a6c3526813c32b05ce
shtrix <37964835+projectpc@users.noreply.github.com>
2020-01-10 01:56:04 +0300

    ss

--- arch_kde_settings.sh
+++ arch_kde_settings.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
 username=$1
-
-####################################################
-#            Настройка системы                     #
-####################################################
-#Добавляю русский язык и смену раскладки alt->shift
-su $username -c "echo '[Layout]
-DisplayNames=,
-LayoutList=us,ru
-LayoutLoopCount=-1
-Model=pc101
-Options=grp:alt_shift_toggle
-ResetOldOptions=true
-ShowFlag=true
-ShowLabel=true
-ShowLayoutIndicator=true
-ShowSingle=false
-SwitchMode=Global
-Use=true' > /home/$username/.config/kxkbrc"
-
-#Авто логин
-su $username -c 'sudo  mkdir -p /etc/sddm.conf.d/'
-su $username -c "sudo printf '[Autologin]
-User=%s
-Session=plasma.desktop' $username >/etc/sddm.conf.d/autologin.conf"
+echo  $username
